90859be3f7fc625964280eedf8c42410b4a981c5
Fabrizio Juarez <fabriziojuarez79@gmail.com>
2025-04-28 21:20:30 -0500

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -5,9 +5,10 @@ require "./vendor/autoload.php";
 date_default_timezone_set("America/Lima");
 
 $params = [
-    'pgsql:host=192.168.1.54;dbname=db_cafeteria',
+    'pgsql:host=192.168.1.54;
+     dbname=db_cafeteria',
     'postgres',
-    '759878'
+    '******'
 ];
 
 Flight::register('db', 'PDO', $params);
